9c59f0181ca832560ad3cd24ebc7495d90134917
nubxilla <blamsonwt@gmail.com>
2017-02-04 15:44:19 -0500

    Update dialogueFunc.js

--- dialogueFunc.js
+++ dialogueFunc.js
@@ -1,4 +1,5 @@
 //WIP: just using a clean file for ease of access, will be added to relevant .js when complete
+//?include function in update for mashing "B" to exit/cancel/back
 
 //on interact with gameObject
 function dialogue()
@@ -9,6 +10,14 @@ function dialogue()
 		surface.drawImage(textBG, 10, 10);
 		surface.fillText("TEXT", 20, 20);
 		//"TEXT" to be replaced with relevant gameObject.text/dialogue
+		//?display lookText first
+		//gameObject.lookText, 20, 20
+		//if interact && lookText displayed && canSpeak = false
+			//remove lookText
+			//gameObject.failSpeak, 20, 20
+		//else
+			//remove lookText
+			//gameObject.dialogue[0], 20, 20
 	};
 	textBG.src = "img/textBG.png";
 };
