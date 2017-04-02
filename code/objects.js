5b6a58b02191d7096cd09438d6038fc2295dc9da
zhutwo <06zhujoh@gmail.com>
2017-04-02 13:56:01 -0400

    Add files via upload
    new NPC class
    new dialog function to be inserted into NPC's talk() method
    removed most old redundant code

--- code/objects.js
+++ code/objects.js
@@ -10,7 +10,7 @@ if (typeof Object.create !== 'function') {
     };
 }
 
-function item (id) // default ui item class, all src images must follow naming convention
+function Item (id) // default ui item class, all src images must follow naming convention
 {
   this.id = id;
   this.canUse = false;
@@ -20,7 +20,7 @@ function item (id) // default ui item class, all src images must follow naming c
   this.oimg.src = "assets/gui/" + id + ".png";
 }
 
-function roomObject (id, x, y, z, tileID, tileIDalt, useType) // default room interactable object class
+function RoomObject (id, x, y, z, tileID, tileIDalt, useType) // default room interactable object class
 {
   this.alt = false;
   this.id = id;
@@ -100,9 +100,37 @@ function roomObject (id, x, y, z, tileID, tileIDalt, useType) // default room in
   };
 }
 
-var goldfish = new item("goldfish");
+function Npc (id, x, y) // NPC class
+{
+  RoomObject.call(this, id, x, y);
+  this.img = new Image;
+  this.img.src ="assets/background/"+ this.id +".png"; // to be changed to sprite folder with sprite sheets
+  this.text.reg.use = "I could get in trouble with HR if I do that.";
+  this.text.reg.look = "It's " + id + ".";
+  this.dlogIdx = 0;
+  this.dlog = [];
+  this.talk = function()
+  {
+    // dialogue function
+  };
+}
+Npc.prototype = Object.create(RoomObject.prototype);
 
-var fishbowl = new roomObject("fishbowl", 6, 7, 2, 124, 134, "container");
+var reception = new Npc("reception", 7, 5);
+reception.text.reg.look = "I could look at that all day.";
+reception.dlog[0] = {	text: "What do you want?",
+                         options:	[	{	reply: "How's your day?", next: 1},
+                                     { reply: "You, in the back seat of my Corolla.", next: 2}]};
+reception.dlog[1] = {	text: "Whatever."};
+reception.dlog[2] = {	text: "In your dreams, creep.",
+                         options:	[	{	reply: "Feelsbadman.", next: 1},
+                                     { reply: "Been there, done that.", next: 3}]};
+reception.dlog[3] = {	text: "Like, eww."};
+reception.init(firstRoom);
+
+var goldfish = new Item("goldfish");
+
+var fishbowl = new RoomObject("fishbowl", 6, 7, 2, 124, 134, "container");
 fishbowl.item = goldfish;
 fishbowl.usedWith = "goldfish";
 fishbowl.text.reg = {
@@ -118,7 +146,7 @@ fishbowl.text.alt = {
 fishbowl.init(firstRoom);
 
 
-var watercooler = new roomObject("watercooler", 9, 8, 2, 125, 126);
+var watercooler = new RoomObject("watercooler", 9, 8, 2, 125, 126);
 watercooler.usedWith = "goldfish";
 watercooler.text.reg = {
   look: "For making uncomfortable small talk.",
@@ -130,3 +158,23 @@ watercooler.text.alt = {
   use: "Now I'm really not thirsty.",
   talk: "This is more tolerable than usual."};
 watercooler.init(firstRoom);
+
+/*  WIP
+
+    var self = new Npc("Me", player.x, player.y);
+    self.text.reg.look = "I'd need a mirror to do that.";
+    self.text.reg.use = "Uh... that can wait until after work.";
+*/
+
+/*  old dialogue stuff
+
+    reception.img = new Image();
+    reception.img.src = "assets/background/reception.png";
+
+    reception.diaText = "Receptionist: What do you want?";
+  	reception.diaText2 = "You:  O -- How's your day?  OR  P -- You in the backseat of my Corolla";
+  	reception.diaText3 = "Receptionist: Whatever.";
+  	reception.diaText4 = "Receptionist: In your dreams, creep.";
+  	reception.diaText5 = "You: U: Feelsbadman.  I: Been there, done that.";
+  	reception.diaText6 = "Receptionist: Like ewww...";
+*/
