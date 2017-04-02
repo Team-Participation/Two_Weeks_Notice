79d7a725764aef404ac79fdf44045474473923e2
zhutwo <06zhujoh@gmail.com>
2017-04-02 18:46:28 -0400

    Add files via upload
    new dialogue function as part of improving npc and gameobject framework
    replaced timedText with dismiss on any input (works for all text prompts)
    removed all old redundant code I came across
    
    textboxes need to be changed to handle more than 1 line, and also be placed over the main canvas (top or bottom, like typical jrpg)

---
+++ game.js
@@ -0,0 +1,102 @@
+/* tiles are set so that when each tile is a square, the aspect
+ * ratio is 16*9. This is just temporary.
+ */
+
+function debug()
+{
+  console.log("!");
+}
+
+var game = {
+    fps: 60,
+    tileSize: 32,
+    width: 1024,
+    height: 576,
+    bgmemory: 0
+};
+
+game.onEachFrame = (function() {
+    var requestAnimationFrame = window.requestAnimationFrame ||
+        window.webkitRequestAnimationFrame ||
+        window.mozRequestAnimationFrame ||
+        window.oRequestAnimationFrame ||
+        window.msRequestAnimationFrame;
+
+    if (requestAnimationFrame) {
+        return function(callback) {
+            var _cb = function() { callback(); requestAnimationFrame(_cb); }
+            _cb();
+        };
+    } else {
+        return function(callback) {
+            setInterval(callback, 1000 / game.fps);
+        }
+    }
+})();
+
+game.start = function() {
+    game.canvas = document.querySelector("canvas");
+    game.canvas.width = game.width;
+    game.canvas.height = game.height;
+    game.context = game.canvas.getContext("2d");
+    game.stage = document.getElementById("gameScreen");
+
+    game.ui = new uiController();
+
+    game.player = new Player();
+
+    game.ui.drawInitial();
+    game.player.room.objects = firstRoom.objects;
+
+    game.onEachFrame(game.run);
+};
+
+game.run = (function() {
+    var loops = 0, skipTicks = 1000 / game.fps,
+        maxFrameSkip = 10,
+        nextgameTick = (new Date).getTime(),
+        lastgameTick;
+
+    return function() {
+        loops = 0;
+
+        while ((new Date).getTime() > nextgameTick) {
+            game.update();
+            nextgameTick += skipTicks;
+            loops++;
+        }
+
+        if (loops) game.draw();
+    }
+})();
+
+game.draw = function() {
+    if(states.currentState == "game"){
+        game.context.clearRect(0, 0, game.width, game.height);
+		//game.player.room.drawRoom();
+		firstRoom.drawBG();
+    game.player.draw(game.context);
+		firstRoom.drawTallBG();
+
+    }else if(states.currentState == "menu"){
+
+        if (menuEst.assetsLoaded == menuEst.numAssetsLoaded()){
+            game.ui.drawMenu();
+        }
+    }else if(states.currentState == "pause"){
+			game.ui.drawPauseMenu();
+		}
+};
+
+game.update = function() {
+	game.ui.checkState();
+    if(states.currentState == "game"){
+        game.player.update();
+    }else if(states.currentState == "menu"){
+        game.ui.updateMenu();
+    }else if(states.currentState == "pause"){
+		game.ui.updateMenu();
+		}
+};
+
+window.onload = game.start;
---
+++ level.js
@@ -0,0 +1,71 @@
+// a class to contain all the info of the current room
+function room ()
+{
+    this.height = 18;
+    this.width = 32;
+    this.tileSize = 32;
+
+    this.walls = [];
+	this.collisionArray = [];
+    this.objects = [];
+	this.BGArray = [];
+	this.tallBG = [];
+}
+
+//var invSpriteSheet = new Image();
+
+var backgroundSpriteSheet = new Image();
+backgroundSpriteSheet.src = "assets/background/newSpriteSheet.png";
+
+
+room.prototype.drawBG = function() {
+	for(var row = 0; row < this.BGArray.length; row++)
+	{
+		for(var col = 0; col < this.BGArray[row].length; col++)
+		{
+			for(var i = 0; i < this.BGArray[row][col].length; i++)
+			{
+				//console.log("sx: ", (this.BGArray[row][col][i] % 10));
+				//console.log("sy: ", (Math.floor(this.BGArray[row][col][i] / 10)));
+				game.context.drawImage(backgroundSpriteSheet,
+							(this.BGArray[row][col][i] % 10) * this.tileSize,
+							(Math.floor(this.BGArray[row][col][i] / 10) * this.tileSize),
+							this.tileSize, this.tileSize,
+							col * this.tileSize, row * this.tileSize,
+							this.tileSize, this.tileSize);
+			}
+		}
+	}
+}
+
+room.prototype.drawTallBG = function() {
+	for(var i = 0; i < this.tallBG.length; i++)
+	{
+		//starts at 1 so it doesn't redraw the floor, just everything else
+		for(var j = 1; j < this.BGArray[this.tallBG[i][1]][this.tallBG[i][0]].length; j++)
+		{
+			game.context.drawImage(backgroundSpriteSheet,
+						(this.BGArray[this.tallBG[i][1]][this.tallBG[i][0]][j] % 10) * this.tileSize,
+						Math.floor(this.BGArray[this.tallBG[i][1]][this.tallBG[i][0]][j] / 10) * this.tileSize,
+						this.tileSize, this.tileSize,
+						this.tallBG[i][0] * this.tileSize, this.tallBG[i][1] * this.tileSize,
+						this.tileSize, this.tileSize);
+		}
+	}
+}
+
+/* function returns false if the player bumps into a wall
+ * playerNextPos needs to be an array containing the coords
+ * of the tile the player is tring to move to
+ */
+room.prototype.collision = function(playerNextPos){
+    if(playerNextPos[0] < firstRoom.width && playerNextPos[0] >= 0 &&
+			playerNextPos[1] <= firstRoom.height && playerNextPos[1] >= 0)
+	{
+		return !firstRoom.collisionArray[playerNextPos[1]][playerNextPos[0]];
+	}
+    else
+	{
+	return false;
+	}
+}
